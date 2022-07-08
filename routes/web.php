cddb8f355aa6cadfb8de3ced48480170b3bc89e9
webapp99 <codewithpranta@gmail.com>
2022-07-08 22:16:58 +0600

    raw php file inside view

--- routes/web.php
+++ routes/web.php
@@ -15,22 +15,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/hello', function(){
-    return response('<h1>Hello World!</h1>', 200)
-           ->header('Content-Type', 'text/plain')
-           ->header('foo', 'bar');
-});
-
-Route::get('/posts/{id}', function($id){
-    //dd($id);
-    //ddd($id);
-    return response('Post ' . $id);
-})->where('id', '[0-9]+');
-
-Route::get('/search', function(Request $request){
-   //dd($request->name . ' '. $request->city);
-   return $request->name . ' '. $request->city;
+    return view('listings');
 });
