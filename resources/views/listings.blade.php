a4b67336be448379a97dca7d29b98d02876a37ee
webapp99 <codewithpranta@gmail.com>
2022-07-10 23:05:53 +0600

    Front template and view part

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,25 +1,54 @@
-<h1>{{$heading}}</h1>
-{{--
-@php
+@extends('layout')
 
-@endphp
---}}
+@section('content')
 
-{{--
-@if(count($listings)== 0)
-<p>No listings found</p>
-@endif
---}}
+@include('partials._hero')
+@include('partials._search')
 
 @unless(count($listings) == 0)
 @foreach($listings as $listing)
-<h2>
-    <a href="/listings/{{$listing['id']}}">{{$listing['title']}}</a>
-</h2>
-<p>
-    {{$listing['description']}}
-</p>
+                <div class="bg-gray-50 border border-gray-200 rounded p-6">
+                    <div class="flex">
+                        <img
+                            class="hidden w-48 mr-6 md:block"
+                            src="{{asset('images/stark.png')}}"
+                            alt=""
+                        />
+                        <div>
+                            <h3 class="text-2xl">
+                                <a href="/listings/{{$listing->id}}">{{$listing->title}}</a>
+                            </h3>
+                            <div class="text-xl font-bold mb-4">
+                                {{$listing->company}}
+                            </div>
+                            <ul class="flex">
+                                <li
+                                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs">
+                                    <a href="#">Laravel</a>
+                                </li>
+                                <li
+                                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs">
+                                    <a href="#">API</a>
+                                </li>
+                                <li
+                                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs">
+                                    <a href="#">Backend</a>
+                                </li>
+                                <li
+                                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs">
+                                    <a href="#">Vue</a>
+                                </li>
+                            </ul>
+                            <div class="text-lg mt-4">
+                                <i class="fa-solid fa-location-dot"></i>
+                                {{$listing->location}}
+                            </div>
+                        </div>
+                    </div>
+                </div>
 @endforeach
 @else
 <p>No Listing found</p>
 @endunless
+@endsection
+
--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,13 @@ Route::get('/', function () {
 
 // Single listings
 Route::get('/listings/{id}', function($id){
-    return view('listing', [
-        'listing' => Listing::find($id)
-    ]);
+    $listing = Listing::find($id);
+
+    if($listing){
+        return view('listing', [
+            'listing' => $listing
+        ]);
+    }else{
+        abort(404);
+    }
 });
